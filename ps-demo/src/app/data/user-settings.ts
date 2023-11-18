164fe14da340601bb5bee0a2f8a585cf899bd74c
Scot Pfuntner <spinatose@gmail.com>
2023-11-18 13:51:45 -0600

    added some validation and other form fields

---
+++ ps-demo/src/app/data/user-settings.ts
@@ -0,0 +1,8 @@
+
+export interface UserSettings {
+  name: string|null,
+  emailOffers: string|null,
+  interfaceStyle: string|null,
+  subscriptionType: string|null,
+  notes: string|null
+}
--- ps-demo/src/app/user-settings-form/user-settings-form.component.ts
+++ ps-demo/src/app/user-settings-form/user-settings-form.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-user-settings-form',
   templateUrl: './user-settings-form.component.html',
   styleUrls: ['./user-settings-form.component.css']
 })
-export class UserSettingsFormComponent {
+export class UserSettingsFormComponent implements OnInit {
 
+  originalUserSettings: UserSettings = {
+    emailOffers: null,
+    interfaceStyle: null,
+    name: null,
+    notes: null,
+    subscriptionType: null
+  }
+  userSettings!: UserSettings ;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.userSettings = { ...this.originalUserSettings };
+  }
+
+  onBlur(field: NgModel) {
+    console.log('in onBlur: ', field.valid);
+  }
+
+  onSubmit(form: NgForm) {
+    console.log('in onSubmit: ', form.valid);
+  }
 }
